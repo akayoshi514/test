8e600cfcfd4d20fe62c484069fe04562846b0353
akayoshi514 <akayoshi514@gmail.com>
2025-12-24 05:21:47 -0500

    Create BatchReactiveLoomApplication class

---
+++ sec/BatchReactiveLoomApplication.java
@@ -0,0 +1,11 @@
+package com.example.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchReactiveLoomApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BatchReactiveLoomApplication.class, args);
+    }
+}
